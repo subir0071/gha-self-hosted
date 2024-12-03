af10bde0968f01f456fe31a113fceb3bab6ffc08
Subir Chandra Ghosh <ghosh.subir.c@gmail.com>
2024-12-03 15:01:37 -0700

    chore: update backend var

--- create-azure-infra/data.tf
+++ create-azure-infra/data.tf
@@ -1,6 +1,1 @@
-data "azurerm_function_app_host_keys" "gha_runner_ahk" {
-  name                = azurerm_linux_function_app.gha_runner_receiver_function_app.name
-  resource_group_name = azurerm_resource_group.gha_runner_rg.name
-
-  depends_on = [ azurerm_linux_function_app.gha_runner_receiver_function_app ]
-}
+data "azurerm_client_config" "current" {}
