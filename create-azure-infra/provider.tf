aa06752a8f2a1962065ff3c59d9db320cb6a6f56
Subir Chandra Ghosh <ghosh.subir.c@gmail.com>
2024-12-02 22:08:24 -0700

    chore: update backend var

--- create-azure-infra/provider.tf
+++ create-azure-infra/provider.tf
@@ -1,22 +1,3 @@
 provider "azurerm" {
   features {}
 }
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-rg"
-  location = "East US"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacct"  # Unique name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "example" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
-}
