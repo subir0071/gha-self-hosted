1aa99890ee8b24db35fa4b603f2612c49402b14e
Subir Chandra Ghosh <ghosh.subir.c@gmail.com>
2024-12-05 23:15:02 -0700

    fix: correct workflow name

--- create-azure-infra/remote-state/main.tf
+++ create-azure-infra/remote-state/main.tf
@@ -10,11 +10,13 @@ resource "azurerm_resource_group" "terraformbackend"{
 }
 
 resource "azurerm_storage_account" "terraform_backend" {
-    name                = var.storage_account_name
-    resource_group_name = azurerm_resource_group.terraformbackend.name
-    location            = azurerm_resource_group.terraformbackend.location
-    account_tier        = var.storage_account_tier
-    account_replication_type = var.storage_account_replication_type
+    name                            = var.storage_account_name
+    resource_group_name             = azurerm_resource_group.terraformbackend.name
+    location                        = azurerm_resource_group.terraformbackend.location
+    account_tier                    = var.storage_account_tier
+    account_replication_type        = var.storage_account_replication_type
+    public_network_access_enabled   = false
+    allow_nested_items_to_be_public = false
 }
 
 resource "azurerm_storage_container" "terraform_backend" {
