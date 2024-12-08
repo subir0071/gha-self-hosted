c46a78fa45d10e76057ae767d3e9ef97e10b2f9d
Subir Chandra Ghosh <ghosh.subir.c@gmail.com>
2024-12-07 19:42:40 -0700

    chore:Add role assignment

--- create-azure-infra/remote-state/main.tf
+++ create-azure-infra/remote-state/main.tf
@@ -4,23 +4,27 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "terraformbackend"{
+resource "azurerm_resource_group" "terraformbackend_rg"{
   name                  = var.resource_group_name
   location              = var.location
 }
 
-resource "azurerm_storage_account" "terraform_backend" {
+resource "azurerm_storage_account" "terraform_backend_sa" {
     name                            = var.storage_account_name
     resource_group_name             = azurerm_resource_group.terraformbackend.name
     location                        = azurerm_resource_group.terraformbackend.location
     account_tier                    = var.storage_account_tier
     account_replication_type        = var.storage_account_replication_type
-    public_network_access_enabled   = false
-    allow_nested_items_to_be_public = false
 }
 
-resource "azurerm_storage_container" "terraform_backend" {
+resource "azurerm_storage_container" "terraform_backend_sc" {
   name                  = var.storage_container_name 
   storage_account_id    = azurerm_storage_account.terraform_backend.id
   container_access_type = var.container_access_type
+}
+
+resource "azurerm_role_assignment" "terraform_backend_ra" {
+  principal_id         = "<service-principal-object-id>"
+  role_definition_name = "Storage Blob Data Contributor"
+  scope                = azurerm_storage_account.example.id
 }
