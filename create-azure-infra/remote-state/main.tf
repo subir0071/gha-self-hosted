8a04266cb3be0ece661189c169567d853aac2786
Subir Chandra Ghosh <ghosh.subir.c@gmail.com>
2024-12-11 20:53:44 -0700

    feature: Add oidc login to terraform

--- create-azure-infra/remote-state/main.tf
+++ create-azure-infra/remote-state/main.tf
@@ -11,20 +11,14 @@ resource "azurerm_resource_group" "terraformbackend_rg"{
 
 resource "azurerm_storage_account" "terraform_backend_sa" {
     name                            = var.storage_account_name
-    resource_group_name             = azurerm_resource_group.terraformbackend.name
-    location                        = azurerm_resource_group.terraformbackend.location
+    resource_group_name             = azurerm_resource_group.terraformbackend_rg.name
+    location                        = azurerm_resource_group.terraformbackend_rg.location
     account_tier                    = var.storage_account_tier
     account_replication_type        = var.storage_account_replication_type
 }
 
 resource "azurerm_storage_container" "terraform_backend_sc" {
   name                  = var.storage_container_name 
-  storage_account_id    = azurerm_storage_account.terraform_backend.id
+  storage_account_id    = azurerm_storage_account.terraform_backend_sa.id
   container_access_type = var.container_access_type
-}
-
-resource "azurerm_role_assignment" "terraform_backend_ra" {
-  principal_id         = "<service-principal-object-id>"
-  role_definition_name = "Storage Blob Data Contributor"
-  scope                = azurerm_storage_account.example.id
 }
