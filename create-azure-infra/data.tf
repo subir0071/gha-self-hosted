ddd38db86d117e04cf83059f8bcd163f05c432db
Subir Chandra Ghosh <ghosh.subir.c@gmail.com>
2024-12-22 14:15:09 -0700

    fix: Correct az container instance role

--- create-azure-infra/data.tf
+++ create-azure-infra/data.tf
@@ -1,7 +1,8 @@
 
 # Azure Container Instance Contributor Role Definition
 data "azurerm_role_definition" "aci_contributor" {
-  name = "Azure Container Instance Contributor"
+  name = "Azure Container Instances Contributor Role"
 }
 
 data "azurerm_client_config" "current" {}
+
