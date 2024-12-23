5e7ee79d39f689d2c72663c085b71c15fca91847
Subir Chandra Ghosh <ghosh.subir.c@gmail.com>
2024-12-22 17:04:42 -0700

    fix: recover soft deleted key to false

--- create-azure-infra/provider.tf
+++ create-azure-infra/provider.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   features {
     key_vault {
       purge_soft_delete_on_destroy          = true
-      recover_soft_deleted_key_vaults       = true
+      recover_soft_deleted_key_vaults       = false
     }
     resource_group {
        prevent_deletion_if_contains_resources = false
