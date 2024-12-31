ed4d32a57acac39e0e929599e86456a2bb54a809
Subir Chandra Ghosh <ghosh.subir.c@gmail.com>
2024-12-31 13:54:06 -0700

    fix:correct sending proper label to queue

--- github-runner-receiver-function/function_app.py
+++ github-runner-receiver-function/function_app.py
@@ -20,13 +20,12 @@ def receiver_function(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpRes
     request_action = req_body.get('action')
     if request_action != 'queued':
         return func.HttpResponse(f"No runner created for {request_action} webhook", status_code=200)
-    queue_dict = {"label": req_body.get('labels')}
-    msg
-
-
+    
+    label_list = {"labels": req_body.get('workflow_job',{}).get('labels', [])}
+    
     try:
         # Serialize the JSON to a string to send to the queue
-        message = json.dumps(queue_dict)
+        message = json.dumps(label_list)
 
         # Send the message to the queue
         msg.set(message)
@@ -37,23 +36,3 @@ def receiver_function(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpRes
         logging.error(f"Failed to process the request: {str(e)}")
         return func.HttpResponse(f"Failed to send message: {str(e)}", status_code=500)
     
-
-
-    # logging.info('Python HTTP trigger function processed a request.')
-
-    # name = req.params.get('name')
-    # if not name:
-    #     try:
-    #         req_body = req.get_json()
-    #     except ValueError:
-    #         pass
-    #     else:
-    #         name = req_body.get('name')
-
-    # if name:
-    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    # else:
-    #     return func.HttpResponse(
-    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-    #          status_code=200
-    #     )
