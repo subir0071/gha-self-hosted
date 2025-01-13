bdf47ecf64ab3f859a0319085394f4ad52c9e43a
Subir Chandra Ghosh <ghosh.subir.c@gmail.com>
2025-01-12 22:36:54 -0700

    fix: revert to non termination

--- github-runner-receiver-function/function_app.py
+++ github-runner-receiver-function/function_app.py
@@ -2,9 +2,6 @@ import azure.functions as func
 import logging
 import os
 import json
-from azure.identity import DefaultAzureCredential
-from azure.mgmt.containerservice import ContainerInstanceManagementClient
-from azure.mgmt.containerservice.models import ContainerGroup
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 @app.route(route="receiver_function")
@@ -14,23 +11,28 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
     connection="storageAccountConnectionString"
 )
 def receiver_function(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpResponse:
-    req_body = None
+
     try:
         # Parse the JSON payload from the request
         req_body = req.get_json()
     except ValueError:
         return func.HttpResponse("Invalid JSON payload.", status_code=400)
     request_action = req_body.get('action')
-    if request_action == 'queued':
-      workflow_job = req_body.get('workflow_job',{})    
-      try:
+    if request_action != 'queued':
+        return func.HttpResponse(f"No runner created for {request_action} webhook", status_code=200)
+    
+    workflow_job = req_body.get('workflow_job',{})
+    
+    try:
         # Serialize the JSON to a string to send to the queue
         message = json.dumps(workflow_job)
+
         # Send the message to the queue
         msg.set(message)
+
         logging.info(f"Message successfully sent to the queue.")
         return func.HttpResponse("Message successfully sent to the queue.", status_code=200)
-      except Exception as e:
+    except Exception as e:
         logging.error(f"Failed to process the request: {str(e)}")
         return func.HttpResponse(f"Failed to send message: {str(e)}", status_code=500)
     
