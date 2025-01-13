e1a6b0c6ad7ba0b3764e8d92c298bcbad1d35350
Subir Chandra Ghosh <ghosh.subir.c@gmail.com>
2025-01-12 22:21:39 -0700

    feat: Add app param for receiver

--- github-runner-receiver-function/function_app.py
+++ github-runner-receiver-function/function_app.py
@@ -33,27 +33,4 @@ def receiver_function(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpRes
       except Exception as e:
         logging.error(f"Failed to process the request: {str(e)}")
         return func.HttpResponse(f"Failed to send message: {str(e)}", status_code=500)
-    elif request_action == 'completed':
-      SUBSCRIPTION_ID = os.getenv("AZURE_SUBSCRIPTION_ID")
-      RESOURCE_GROUP_NAME = os.getenv("AZURE_RESOURCE_GROUP")
-      credential = DefaultAzureCredential()
-      client = ContainerInstanceManagementClient(credential, SUBSCRIPTION_ID)
-      container_groups = client.container_groups.list_by_resource_group(RESOURCE_GROUP_NAME)
-      deleted_container_count = 0
-
-      for container_group in container_groups:
-    # Check if the container group has terminated (or any other status you want)
-        if container_group.instance_view.state == "Terminated":
-          print(f"Terminated container group found: {container_group.name}")
-        
-        # Delete the terminated container group
-          delete_operation = client.container_groups.begin_delete(RESOURCE_GROUP_NAME, container_group.name)
-          delete_operation.wait()  # Wait for the deletion to complete
-          print(f"Deleted terminated container group: {container_group.name}")
-          deleted_container_count=deleted_container_count+1
-      return func.HttpResponse(f"{deleted_container_count} container(s) deleted", status_code=200)
-    else:
-       return func.HttpResponse(f"No container created for {request_action} action webhook", status_code=200)
-
-       
     
