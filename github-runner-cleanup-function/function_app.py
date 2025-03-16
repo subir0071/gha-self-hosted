6bb2d34908c0ea9592762c50c1c620f8f2f82b2a
Subir Ghosh <ghosh.subir.c@gmail.com>
2025-03-15 23:54:29 -0600

    fix: cleanup lambda

--- github-runner-cleanup-function/function_app.py
+++ github-runner-cleanup-function/function_app.py
@@ -4,6 +4,7 @@ import datetime
 import azure.functions as func
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.containerinstance import ContainerInstanceManagementClient
+from azure.functions import TimerRequest
 
 
 
@@ -11,32 +12,46 @@ from azure.mgmt.containerinstance import ContainerInstanceManagementClient
 app = func.FunctionApp()
 
 
-SUBSCRIPTION_ID = os.getenv("AZURE_SUBSCRIPTION_ID")
-RESOURCE_GROUP_NAME = os.getenv("AZURE_RESOURCE_GROUP")
+
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
 # Set up Azure client
-credential = DefaultAzureCredential()
-container_client = ContainerInstanceManagementClient(credential,"c540fdb6-d9c4-4d9e-9f1d-91743899df62")
+
 
 # Timer-triggered function that checks and deletes terminated containers every 5 minutes
 @app.function_name(name="DeleteTerminatedContainers")
 @app.schedule(schedule="0 */2 * * * *", arg_name="mytimer", run_on_start=True)
-def main(func.TimerRequest) -> None:
+def main(mytimer: func.TimerRequest) -> None:
+    SUBSCRIPTION_ID = os.getenv("AZURE_SUBSCRIPTION_ID")
+    RESOURCE_GROUP_NAME = os.getenv("AZURE_RESOURCE_GROUP")
 
+    logging.info(f"Subscription ID: {SUBSCRIPTION_ID}")
+    
+    logging.info(f"Resource Group: {RESOURCE_GROUP_NAME}")
+
+    credential = DefaultAzureCredential()
+    container_client = ContainerInstanceManagementClient(credential,SUBSCRIPTION_ID)
     try:
         # List all container groups in the specified resource group
-        container_groups = container_client.container_groups.list_by_resource_group(RESOURCE_GROUP_NAME)
-
-        for group in container_groups:
+        container_groups_list = container_client.container_groups.list_by_resource_group(RESOURCE_GROUP_NAME)
+        
+        logging.info(f"Checking for terminated containers {container_groups_list}")
+        for group in container_groups_list:
+            container_group = container_client.container_groups.get(RESOURCE_GROUP_NAME,
+                                                      group.name)
             # Check if the container group has any containers that are terminated
-            for container in group.containers:
+            logging.info(f"for each container group (few params) {group}")
+            logging.info(f"for each container group (after get params) {container_group}")
+            
+            for container in container_group.containers:
                 # If container state is "terminated", delete the container group
-                if container.instance_view.state == 'Terminated':
+                
+                logging.info(f"for each container {container} and state is {container.instance_view.current_state.state}")
+                if container.instance_view.current_state.state == 'Terminated':
                     logging.info(f"Container '{container.name}' has terminated. Deleting container group '{group.name}'")
-                    container_client.container_groups.delete(RESOURCE_GROUP_NAME, group.name)
+                    container_client.container_groups.begin_delete(RESOURCE_GROUP_NAME, group.name)
                     logging.info(f"Container group '{group.name}' deleted successfully.")
     except Exception as e:
         logging.error(f"Error while checking/deleting container groups: {str(e)}")
