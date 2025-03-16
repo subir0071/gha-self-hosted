b1dcbbd5b9337aa45f777f53b02725c1c316e2be
Subir Ghosh <ghosh.subir.c@gmail.com>
2025-03-15 18:52:13 -0600

    fix: add clean fn role assgnnt

--- create-azure-infra/main.tf
+++ create-azure-infra/main.tf
@@ -221,8 +221,8 @@ resource "azurerm_linux_function_app" "gha_runner_cleanup_function_app" {
   }
 }
 
-resource "azurerm_role_assignment" "example" {
-  principal_id   = azurerm_function_app.example.identity[0].principal_id
+resource "azurerm_role_assignment" "gha_controller_fn_ra" {
+  principal_id   = azurerm_linux_function_app.gha_runner_cleanup_function_app.identity[0].principal_id
   role_definition_name = "Contributor" 
   scope           = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.gha_runner_rg.name}/providers/Microsoft.ContainerInstance/containergroups"
 }
