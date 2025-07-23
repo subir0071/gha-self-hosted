a35da1541b442e3290b9ea89f6d2c7091a10c28d
Subir Ghosh <ghosh.subir.c@gmail.com>
2025-07-23 17:50:06 -0600

    fix

--- create-azure-infra/main.tf
+++ create-azure-infra/main.tf
@@ -80,8 +80,7 @@ resource "azurerm_linux_function_app" "gha_runner_controller_function_app" {
     "QUEUE_NAME"                      = azurerm_storage_queue.gh_runner_asq.name
     "APPINSIGHTS_INSTRUMENTATIONKEY"  = azurerm_application_insights.gha_runner_aai.instrumentation_key
     "storageAccountConnectionString"  = azurerm_storage_account.gha_runner_sa.primary_connection_string
-    "ENABLE_ORYX_BUILD"              = "true"
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
+    "WEBSITE_RUN_FROM_PACKAGE"        = 1
   }
 
   #zip_deploy_file = "./gha-runner-controller.zip"
